d84272b2b36906b65f802c4db7ea4125f90e859b
ranggaleoo <leorangga30@ymail.com>
2024-01-14 13:48:56 +0700

    add animation when like and unlike

--- components/ListThreads.vue
+++ components/ListThreads.vue
@@ -4,7 +4,7 @@
             <div class="flex flex-col gap-5 p-6" v-if="!timeline.is_ads">
                 <div class="flex w-full">
                     <div class="flex items-center space-x-4">
-                        <UiAvatar class="h-12 w-12 rounded-full" :fallback="getInitials(timeline.name)" />
+                        <UiAvatar class="h-10 w-10 rounded-full" :fallback="getInitials(timeline.name)" />
                         <div class="space-y-2">
                             <h1 class="h-4 text-base font-bold"> {{ timeline.name }} </h1>
                             <h5 class="h-4 text-xs"> {{ getTimeAgo(timeline.timed) }} </h5>
@@ -18,15 +18,21 @@
                     {{ timeline.text_content }}
                 </div>
                 <div class="flex items-center gap-2">
-                    <!-- <IconHeart class="text-red-500"/> -->
                     <UiButton @click="buttonLikeClicked(timeline.timeline_id)"
-                        class="rounded-full bg-clear text-gray-800 dark:text-gray-50 hover:bg-gray-200">
-                        <IconHeart v-if="timeline.is_liked" class="text-red-500" />
-                        <IconHeart v-else />
-                        {{ timeline.total_likes }}
+                        variant="ghost"
+                        class="rounded-full">
+                        <IconHeart :class="{
+                            'text-red-500': likeStates[timeline.timeline_id]?.isLiked ?? timeline.is_liked,
+                            'animate-pulse': likeStates[timeline.timeline_id]?.likeClicked ?? false
+                        }" />
+                        <span :class="{
+                            'animate-pulse': likeStates[timeline.timeline_id]?.likeClicked ?? false
+                        }"
+                        >{{ timeline.total_likes }}</span>
                     </UiButton>
                     <UiButton @click="goToComment(timeline.timeline_id)"
-                        class="rounded-full bg-clear text-gray-800 dark:text-gray-50 hover:bg-gray-200">
+                        variant="ghost"
+                        class="rounded-full">
                         <IconMessageCircle />{{ timeline.total_comments }}
                     </UiButton>
                     <div class="ml-auto">
@@ -47,6 +53,7 @@ import type { BaseResponse, Timeline } from '@/types/timeline';
 
 const base_url = useRuntimeConfig().public.base_api_url;
 const device_id = ref('B6961C40-5D18-48FE-B06C-1314B34162CC')
+const likeStates = reactive<{ [key: number]: { isLiked: boolean, likeClicked: boolean } }>({});
 
 const props = defineProps<{
     timelines: Timeline[]
@@ -57,9 +64,20 @@ function goToComment(timeline_id: number) {
 }
 
 function buttonLikeClicked(timeline_id: number) {
+    if (!likeStates[timeline_id]) {
+        likeStates[timeline_id] = {
+            isLiked: false,
+            likeClicked: false
+        };
+    }
+
     const item = props.timelines.filter(item => item.timeline_id == timeline_id)
+    likeStates[timeline_id].isLiked = item.at(0)?.is_liked ?? false
+    likeStates[timeline_id].likeClicked = true;
+    
     console.log(props.timelines)
-    if (item[0].is_liked ?? false) {
+
+    if (likeStates[timeline_id].isLiked) {
         console.log('perform unlike')
         doUnlikeTimeline(timeline_id)
     } else {
@@ -82,14 +100,17 @@ const doLikeTimeline = async (timeline_id: number) => {
         );
         console.log(responseLike)
         if (responseLike.success) {
-            console.log(responseLike.message)
+            likeStates[timeline_id].isLiked = true;
+            const item = props.timelines.filter(item => item.timeline_id == timeline_id)
+            item.at(0)!.is_liked = true
+            item.at(0)!.total_likes += 1
         } else {
             console.log(responseLike.message)
         }
     } catch (error) {
         console.log(error)
     } finally {
-
+        likeStates[timeline_id].likeClicked = false;
     }
 }
 
@@ -107,14 +128,19 @@ const doUnlikeTimeline = async (timeline_id: number) => {
         );
         console.log(responseUnlike)
         if (responseUnlike.success) {
-            console.log(responseUnlike.message)
+            likeStates[timeline_id].isLiked = false;
+            const item = props.timelines.filter(item => item.timeline_id == timeline_id)
+            item.at(0)!.is_liked = false
+            if(item.at(0)?.total_likes ?? 0 > 0) {
+                item.at(0)!.total_likes -= 1
+            }
         } else {
             console.log(responseUnlike.message)
         }
     } catch (error) {
         console.log(error)
     } finally {
-
+        likeStates[timeline_id].likeClicked = false;
     }
 }
 
